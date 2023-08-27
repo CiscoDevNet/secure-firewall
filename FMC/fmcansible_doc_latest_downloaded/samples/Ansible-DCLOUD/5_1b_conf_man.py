356d60a3baeece652a21d98fa2cb962c7870a60b
kadadhic <kadadhic@cisco.com>
2023-08-27 11:17:51 +0530

    minor-change

--- FMC/fmcansible_doc_latest_downloaded/samples/Ansible-DCLOUD/5_1b_conf_man.py
+++ FMC/fmcansible_doc_latest_downloaded/samples/Ansible-DCLOUD/5_1b_conf_man.py
@@ -1,9 +1,9 @@
 import Devmiko
 import getpass
 
-host = "3.142.207.89"
+host = "<Host_IP>"
 username = "admin"
-password = "Cisco@123"
+password = "<Password>"
 
 client = Devmiko.FTDClient(debug=False, filename=None, level='DEBUG')
 client.connect(host, username=username, password=password)
@@ -12,6 +12,6 @@ client.send_command(command='configure firewall transparent')
 client.send_command(command='y')
 client.send_command(command='configure firewall routed')
 client.send_command(command='y')
-client.send_command(command='configure manager add 3.38.170.214 cisco123')
+client.send_command(command='configure manager add <Host_ip> cisco123')
 print(client.output)
 client.disconnect()
