25590346855684f9f67d9d08b4b849788b913962
Mukesh Sharma <mukeshsharma24@gmail.com>
2023-10-01 02:13:18 +1300

    Implemented templatefile() for the user_data scripts in  ftdv and fmcv aws instance resources

--- AWS_Workshop/AWS_Workshop_Code/module/FirewallServer/main.tf
+++ AWS_Workshop/AWS_Workshop_Code/module/FirewallServer/main.tf
@@ -1,43 +1,56 @@
 # # ##################################################################################################################################
 # # # Create the Cisco FMC and FTD Instances
 # # ##################################################################################################################################
+locals {
+  rendered_ftd_startup_files = [
+    for idx in range(var.instances_per_az * var.availability_zone_count) : templatefile("${path.module}/ftd_startup_file.txt.tfpl", {
+      fmc_ip     = var.fmc_mgmt_ip,
+      fmc_nat_id = var.fmc_nat_id,
+      reg_key    = var.reg_key
+    })
+  ]
+}
 
 resource "aws_instance" "ftdv" {
-  count               = var.instances_per_az * var.availability_zone_count
-  ami                 = data.aws_ami.ftdv.id
-  instance_type       = var.ftd_size
-  key_name            = var.keyname
+  count         = var.instances_per_az * var.availability_zone_count
+  ami           = data.aws_ami.ftdv.id
+  instance_type = var.ftd_size
+  key_name      = var.keyname
   network_interface {
-    network_interface_id = element(var.ftd_mgmt_interface,count.index)
+    network_interface_id = element(var.ftd_mgmt_interface, count.index)
     device_index         = 0
   }
   network_interface {
-    network_interface_id = element(var.ftd_diag_interface,count.index)
+    network_interface_id = element(var.ftd_diag_interface, count.index)
     device_index         = 1
   }
   network_interface {
-    network_interface_id = element(var.ftd_outside_interface,count.index)
+    network_interface_id = element(var.ftd_outside_interface, count.index)
     device_index         = 2
   }
   network_interface {
-    network_interface_id = element(var.ftd_inside_interface,count.index)
+    network_interface_id = element(var.ftd_inside_interface, count.index)
     device_index         = 3
   }
-  user_data = data.template_file.ftd_startup_file[count.index].rendered
+  user_data = local.rendered_ftd_startup_files[count.index]
   tags = merge({
     Name = "Cisco ftdv${count.index}"
-  },var.tags)
+  }, var.tags)
 }
 
 resource "aws_instance" "fmcv" {
-  ami                 = data.aws_ami.fmcv.id
-  instance_type       = "c5.4xlarge"
-  key_name            = var.keyname   
+  ami           = data.aws_ami.fmcv.id
+  instance_type = "c5.4xlarge"
+  key_name      = var.keyname
   network_interface {
     network_interface_id = var.fmcmgmt_interface
     device_index         = 0
   }
-  user_data = data.template_file.fmc_startup_file.rendered
+  user_data = base64encode(templatefile("${path.module}/fmc_startup_file.txt.tfpl",
+    {
+      fmc_admin_password = var.fmc_admin_password
+      fmc_hostname       = var.fmc_hostname
+  }))
   tags = {
     Name = "Cisco FMCv"
   }
