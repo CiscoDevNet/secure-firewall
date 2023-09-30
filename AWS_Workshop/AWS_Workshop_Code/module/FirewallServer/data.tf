5dc0669897b367573b06b6e8f7379170671cdd4c
Mukesh Sharma <mukeshsharma24@gmail.com>
2023-10-01 02:11:15 +1300

    Removed template_file data resources as its deprecated

--- AWS_Workshop/AWS_Workshop_Code/module/FirewallServer/data.tf
+++ AWS_Workshop/AWS_Workshop_Code/module/FirewallServer/data.tf
@@ -1,8 +1,8 @@
 data "aws_ami" "ftdv" {
   #most_recent = true      // you can enable this if you want to deploy more
-  owners      = ["aws-marketplace"]
+  owners = ["aws-marketplace"]
 
- filter {
+  filter {
     name   = "name"
     values = ["${var.FTD_version}*"]
   }
@@ -18,19 +18,10 @@ data "aws_ami" "ftdv" {
   }
 }
 
-data "template_file" "ftd_startup_file" {
-    count = var.instances_per_az * var.availability_zone_count
-    template = file("${path.module}/ftd_startup_file.txt")
-    vars = {
-    fmc_ip       = var.fmc_mgmt_ip
-    fmc_nat_id   = var.fmc_nat_id
-    reg_key      = var.reg_key
-    }
-}
 
 data "aws_ami" "fmcv" {
   #most_recent = true      // you can enable this if you want to deploy more
-  owners      = ["aws-marketplace"]
+  owners = ["aws-marketplace"]
   filter {
     name   = "name"
     values = ["${var.FMC_version}*"]
@@ -45,15 +36,7 @@ data "aws_ami" "fmcv" {
   }
 }
 
-data "template_file" "fmc_startup_file" {
-  template = file("${path.module}/fmc_startup_file.txt")
-  vars = {
-    fmc_admin_password       = var.fmc_admin_password
-    fmc_hostname             = var.fmc_hostname
-  }
-}
-
 data "aws_availability_zones" "available" {
-    state = "available"
+  state = "available"
 }
 
