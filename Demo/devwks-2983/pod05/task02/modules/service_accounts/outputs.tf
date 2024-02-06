c65b0d3174eff5a02906c0e56509c91a882f694b
jeroenwittock <jeroenwittock@gmail.com>
2024-02-06 09:13:59 +0100

    removing pods

--- Demo/devwks-2983/pod05/task02/modules/service_accounts/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "email" {
-  value       = google_service_account.sa.email
-  description = "The email address of the service account."
-}
--- Demo/devwks-2983/pod07/task02/modules/vm/locals.tf
+++
@@ -1,7 +0,0 @@
-# ###############
-# Locals
-###############
-locals {
-  #vm_ips_ftd = [for x in google_compute_instance.ftd : x.network_interface.0.access_config.0.nat_ip]
-  vm_ips_fmc = try([google_compute_instance.fmc[*].network_interface.0.access_config.0.nat_ip], [])
-}
